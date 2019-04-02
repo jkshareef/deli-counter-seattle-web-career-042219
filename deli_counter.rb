aaa18a49cd417e3b592788393edf5d6172fc143e
Jamal Shareef <theepie@hotmail.com>
2019-04-02 05:57:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,6 @@ end
 def now_serving(array)
   if array.size == 0
     puts "There is nobody waiting to be served!"
-  end
-  puts "Currently serving #{array.shift}."
+  else
+    puts "Currently serving #{array.shift}."
 end
