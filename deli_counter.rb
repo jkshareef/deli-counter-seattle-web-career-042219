7d6faaa27de01ca1839f778ec8a54ff007d903a5
Jamal Shareef <theepie@hotmail.com>
2019-04-02 06:08:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def line(array)
+  message = "The line is currently:"
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    array.each_with_index { |name, index|
+      message += " #{index+1}. #{name}"
+    }
+    puts message
+  end
+end
+
+def take_a_number(array, name)
+  puts "Welcome, #{name}. You are number #{(array.push(name)).size} in line."
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
+end
